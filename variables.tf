4c3b82ea77c56bf64c5c3453794a7472a29b39a8
OpsDev <ops2dev@hotmail.com>
2023-06-07 21:36:08 +0700

    Update

--- variables.tf
+++ variables.tf
@@ -6,14 +6,9 @@ variable "TENANT_ID" {
   type = string
 }
 
-variable "RSG_ID" {
-  type = string
-
-  validation {
-    condition     = regex(["^[a-zA-Z0-9_-]*$"], var.RSG_ID)
-    error_message = "Err: valid cloud options are: 'azure', 'aws', 'gcp'."
-  }
-}
+#variable "RSG_ID" {
+#  type = string
+#}
 
 variable "LOCATION_ID" {
   type = string
