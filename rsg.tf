04da3df1d20bb681edbae6048496736319a501c4
OpsDev <ops2dev@hotmail.com>
2023-06-07 21:42:28 +0700

    Update rsg.tf

--- rsg.tf
+++ rsg.tf
@@ -1,6 +1,11 @@
 
 variable "RSG_ID" {
   type = string
+
+    validation {
+    condition     = can(regex("^[0-9A-Za-z]+$", var.RSG_ID))
+    error_message = "The server name variable must have letters and numbers only"
+  }
 }
 resource "azurerm_resource_group" "rsg" {
   name     = "${var.RSG_ID}"
