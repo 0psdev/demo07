0f962b2b08869cfd4dc6f98925866aacd5ee80a9
OpsDev <ops2dev@hotmail.com>
2023-06-07 18:21:21 +0700

    Update

--- variables.tf
+++ variables.tf
@@ -6,8 +6,13 @@ variable "TENANT_ID" {
   type = string
 }
 
-variable "RSG_ID" {
+variable "RG_ID" {
   type = string
+
+  validation {
+    condition     = can(regex("^[0-9A-Za-z]+$", var.RG_ID))
+    error_message = "The server name variable must have letters and numbers only"
+  }
 }
 
 variable "LOCATION_ID" {
