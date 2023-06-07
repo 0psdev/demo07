eeb05b44883087c18b2bee85bab52b670eda2daa
OpsDev <ops2dev@hotmail.com>
2023-06-07 18:05:55 +0700

    Update rsg.tf

--- rsg.tf
+++ rsg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rsg" {
-  name     = regex("^[-\\w\\._\\(\\)]+$",substr("${var.RSG_ID}", 90))
+  name     = regex("^[-\\w\\._\\(\\)]+$",substr(var.RSG_ID, 90))
   location = var.LOCATION_ID
 }
